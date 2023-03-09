d2bfabbdfa0cb6a21362c4b4cf7c355e30bf0904
umairusmankhail <umairusmankhail@gmail.com>
2023-03-09 19:19:01 +0500

    changes by umair

--- app/Http/Controllers/DataTableAjaxCRUDController.php
+++ app/Http/Controllers/DataTableAjaxCRUDController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
 use App\Models\Category;
-use Illuminate\Support\Facades\DB;
+
 use DataTables;
 use Validator;
 use Illuminate\Support\Facades\Hash;
